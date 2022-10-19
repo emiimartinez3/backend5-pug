3528f6389a12c0bda058df555be1c0babb228165
Emiliano Martinez <emilianomartinez1933@gmail.com>
2022-10-19 20:32:27 -0300

    pug

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const { Router } = express
+const routerProducts = Router()
+const containerApi = require ('./src/containerApi') 
+const app = express()
+
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+app.use('/api/products', routerProducts)
+app.use(express.static('public'))
+
+
+const products = []
+let api = new containerApi(products)
+
+routerProducts.get('/', (req, res) => {
+    res.render('products', { products })
+})
+
+routerProducts.post('/', (req, res) => {
+    api.addProduct(req, res);
+})
+
+routerProducts.get('/:id', (req, res) => {
+    api.getProduct(req, res);
+})
+
+routerProducts.put('/:id', (req, res) => {
+    api.modifyProduct(req, res);
+})
+
+routerProducts.delete('/:id', (req, res) => {
+    api.deleteProduct(req, res);
+})
+
+
+
+const PORT = process.env.port || 8080
+
+const server = app.listen(PORT, () => {
+    console.log(`HTTP Server running on port ${server.address().port}`)
+})
+
+server.on("error", error => console.log(`Error on server ${error}`))
